05746959f8a67fbfa34c3f0a3884754ab941ab05
jeremyfelt <jeremy.felt@gmail.com>
2014-08-11 12:56:37 -0700

    Setup initial structure for handling shortcodes

--- wsuwp-content-syndicate.php
+++ wsuwp-content-syndicate.php
@@ -8,8 +8,42 @@ Version: 0.0.1
 */
 
 class WSU_Content_Syndicate {
+	/**
+	 * Setup hooks for shortcodes.
+	 */
 	public function __construct() {
+		add_shortcode( 'wsuwp_json', array( $this, 'display_wsuwp_json' ) );
+		add_shortcode( 'wsuwp_elastic', array( $this, 'display_wsuwp_elastic' ) );
+	}
 
+	/**
+	 * Process the requested parameters for use with the WordPress JSON API and output
+	 * the response accordingly.
+	 *
+	 * @return string
+	 */
+	public function display_wsuwp_json() {
+		ob_start();
+
+		$content = ob_get_contents();
+		ob_end_clean();
+
+		return $content;
+	}
+
+	/**
+	 * Process the requested parameters for use with WSU Search via Elasticsearch and output
+	 * the response accordingly.
+	 *
+	 * @return string
+	 */
+	public function display_wsuwp_elastic() {
+		ob_start();
+
+		$content = ob_get_contents();
+		ob_end_clean();
+
+		return $content;
 	}
 }
 new WSU_Content_Syndicate();
