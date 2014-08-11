a490979968fd4e93968910dac164f2683ab5ac1c
jeremyfelt <jeremy.felt@gmail.com>
2014-08-11 12:52:11 -0700

    Add initial files for plugin

---
+++ README.md
@@ -0,0 +1,3 @@
+# WSU Content Syndicate
+
+A WordPress plugin to retrieve and display content from throughout Washington State University
---
+++ wsuwp-content-syndicate.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WSU Content Syndicate
+Plugin URI: http://web.wsu.edu
+Description: Retrieve content for display from throughout Washington State University
+Author: washingtonstateuniversity, jeremyfelt
+Version: 0.0.1
+*/
+
+class WSU_Content_Syndicate {
+	public function __construct() {
+
+	}
+}
+new WSU_Content_Syndicate();
