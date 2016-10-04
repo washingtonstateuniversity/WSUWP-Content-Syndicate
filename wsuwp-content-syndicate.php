8d308c259f46dcb7901439f28d40236d32c86c72
Jeremy Felt <jeremy.felt@gmail.com>
2016-10-03 18:17:00 -0700

    Bump version 0.9.3

--- wsuwp-content-syndicate.php
+++ wsuwp-content-syndicate.php
@@ -5,7 +5,7 @@ Plugin URI: https://web.wsu.edu/wordpress/plugins/wsuwp-content-syndicate/
 Description: Retrieve content for display from throughout Washington State University
 Author: washingtonstateuniversity, jeremyfelt
 Author URI: https://web.wsu.edu/
-Version: 0.9.2
+Version: 0.9.3
 */
 
 class WSU_Content_Syndicate {
