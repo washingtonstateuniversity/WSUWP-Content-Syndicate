56955e54b3298c1484db9945037fb36df078f687
Jeremy Felt <jeremy.felt@gmail.com>
2017-05-15 19:08:39 -0700

    Update plugin header with better description, authors

--- wsuwp-content-syndicate.php
+++ wsuwp-content-syndicate.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: WSUWP Content Syndicate
 Plugin URI: https://web.wsu.edu/wordpress/plugins/wsuwp-content-syndicate/
-Description: Retrieve content for display from throughout Washington State University
-Author: washingtonstateuniversity, jeremyfelt
+Description: Retrieve content for display from other WordPress sites.
+Author: washingtonstateuniversity, jeremyfelt, philcable
 Author URI: https://web.wsu.edu/
 Version: 1.0.0
 */
