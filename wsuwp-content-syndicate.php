7b57c89d2aab4dd6b1d151a6ffd5fa4d6808ac66
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-04 10:41:38 -0700

    Correct the plugin name in the PHP 5.3 error message

--- wsuwp-content-syndicate.php
+++ wsuwp-content-syndicate.php
@@ -3,7 +3,7 @@
 Plugin Name: WSUWP Content Syndicate
 Plugin URI: https://github.com/washingtonstateuniversity/WSUWP-Content-Syndicate
 Description: Retrieve content for display from other WordPress sites.
-Author: washingtonstateuniversity, jeremyfelt
+Author: washingtonstateuniversity, jeremyfelt, philcable
 Author URI: https://web.wsu.edu/
 Version: 1.3.0
 */
@@ -16,7 +16,7 @@ if ( ! defined( 'WPINC' ) ) {
 // This plugin uses namespaces and requires PHP 5.3 or greater.
 if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
 	add_action( 'admin_notices', create_function( '',
-		"echo '<div class=\"error\"><p>" . __( 'WSUWP Plugin Skeleton requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'wsuwp-plugin-skeleton' ) . "</p></div>';" ) );
+		"echo '<div class=\"error\"><p>" . __( 'WSUWP Content Syndicate requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'wsuwp-content-syndicate' ) . "</p></div>';" ) );
 	return;
 } else {
 	include_once __DIR__ . '/includes/content-syndicate.php';
